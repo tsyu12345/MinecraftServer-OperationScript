621ea2a78121f2a27a05061060ca4af694c9782a
syu_asus_Note <syukunt@icloud.com>
2022-05-29 19:55:02 +0900

    本BOTのコマンド処理をまとめたクラスの作成。

---
+++ commands.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+from typing import Final as const
+
+from Server.MinecraftServer.MinecraftServer import MinecraftServer
+from Server.VPS.vps import VPS
+
+class Commands():
+    """_summary_\n
+    本BOTで実行されるコマンドの集合。
+    """
+    VPS: VPS
+    MC_SERVER: MinecraftServer
+    
+    def __init__(self, vps: VPS, mc_server: MinecraftServer) -> None:
+        """_summary_\n
+        Arguments:\n
+            bot {DiscordBot} -- DiscordBotのインスタンス。
+        """
+        self.VPS = vps
+        self.MC_SERVER = mc_server
+        
+    @classmethod
+    def shutdown(cls) -> None:
+        """_summary_\n
+        worldを閉じてサーバーを停止する。
+        """
+        cls.MC_SERVER.stop()
+        cls.VPS.shutdown()
+    
+    @classmethod
+    def start(cls) -> None:
+        """_summary_\n
+        VPSを起動し、worldを開く。
+        """
+        cls.VPS.boot()
+        #TODO:コマンド実行でjavaコマンド実行
+    
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import json
 
 from Bot.discord_bot import DiscordBot
 from Server.VPS.vps import VPS
+from Server.MinecraftServer.MinecraftServer import MinecraftServer, LoginParam
 from Server.VPS.abstract import ConohaAPIParam
+from commands import Commands
 
 def main() -> None:
     
@@ -15,15 +17,32 @@ def main() -> None:
     vps_param = get_VPS_param()
     vps = VPS(vps_param)
     
+    
+    mc_server = MinecraftServer() #TODO:インスタンスパラメータの調整
+    
+    Commands(vps, mc_server)
+    
     #add commandEvents below
     bot.addCommandListener("boot", vps.boot)
     bot.addCommandListener("shutdown", vps.shutdown)
     bot.addCommandListener("force_shutdown", vps.force_shutdown)
-    
+    bot.addCommandListener("start", Commands.start)
     #starting bot
     bot.run()
 
 
+def get_rcon_param() -> LoginParam:
+    
+    with json.load(open("./Server/ServerConfig.json")) as config_file:
+        param = LoginParam(
+        config_file["rcon"]["ipv4"],
+        config_file["rcon"]["port"],
+        config_file["rcon"]["password"]
+        )
+        
+        return param
+    
+    
 def get_VPS_param() -> ConohaAPIParam:
     
     with json.load(open("./Server/ServerConfig.json")) as config_file:
